00a0479986d5a1a656d69602a58702795f9fc528
shaikmohammed8 <Mohammedrazachandwala@gmail.com>
2020-11-23 00:05:30 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/ecommerce/ui/mainscreen/MainFragment.kt
@@ -0,0 +1,41 @@
+package com.example.ecommerce.ui.mainscreen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.ecommerce.R
+import com.example.ecommerce.databinding.FragmentMainBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MainFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding=FragmentMainBinding.inflate(inflater, container, false)
+        binding.button.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_loginFragment)
+        }
+        binding.button2.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_signUpFragment)
+        }
+        return binding.root
+    }
+
+
+    }
